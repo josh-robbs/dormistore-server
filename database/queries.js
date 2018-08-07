50da2d1b3a37c1126593261ff23105ba5c7c40e1
Josh Robbs <joshrobbs@Zuul.local>
2018-08-07 14:29:23 -0600

    adding queries

--- database/queries.js
+++ database/queries.js
@@ -1,1 +1,28 @@
 const database = require('./database-connection')
+
+module.exports = {
+  listAll(){
+    return database('products').select()
+  },
+  readOne(id){
+    return database('products').select().where('id', id)
+  },
+  postOne(product){
+    return database('products')
+      .insert(product)
+      .returning('*')
+      .then(record => record[0])
+  },
+  deleteOne(id){
+    return database('products')
+      .delete()
+      .where('id', id)
+  },
+  update(id, product){
+    return database('products')
+      .update(product)
+      .where('id', id)
+      .returning('*')
+      .then(record => record[0])
+  }
+}
